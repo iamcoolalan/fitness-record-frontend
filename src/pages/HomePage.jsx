b656621ff5b41b742999fa71daaeb84ab9b2bb5a
iamcoolalan <iamalan1996@gmail.com>
2023-10-31 20:03:32 +0800

    feat: add select day mode static view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 import clsx from 'clsx'
+import addDays from 'date-fns/addDays'
+import subDays from 'date-fns/subDays'
 
-function toDateString (date) {
-  return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
-}
+import Calender from '../components/Calendar.jsx'
 
 const HomePage = () => {
   const today = new Date()
+  const startDayForDayModeInitialValue = new Date(today)
+  startDayForDayModeInitialValue.setDate(today.getDate() - 3)
 
   const yearInputRef = useRef(null)
   const monthInputRef = useRef(null)
@@ -18,6 +20,18 @@ const HomePage = () => {
   const [isMonthEdit, setIsMonthEdit] = useState(false)
   const [monthInputValue, setMonthInputValue] = useState(month)
   const [yearInputValue, setYearInputValue] = useState(year)
+  const [selectedDate, setSelectedDate] = useState(new Date())
+  const [startDayForDayMode, setStartDayForDayMode] = useState(startDayForDayModeInitialValue)
+
+  function getDayOfWeek(date) {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    return daysOfWeek[date.getDay()]
+  }
+
+  function toDateString (date) {
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+  }
+
 
   function handleYearLeftClick() {
     setYear(year - 1)
@@ -96,6 +110,46 @@ const HomePage = () => {
     cb(e.target.value);
   }
 
+  function handleSelectedDateClick (selectedDate) {
+    if ((selectedDate.getMonth() + 1) !== month ) {
+      setMonth(selectedDate.getMonth() + 1)
+    }
+
+    if (selectedDate.getFullYear() !== year) {
+      setYear(selectedDate.getFullYear())
+    }
+
+    setSelectedDate(selectedDate)
+  }
+
+  function handleSelectedPreviousDateClick() {
+    const setDate = subDays(selectedDate, 1)
+
+    if ((setDate.getMonth() + 1) !== month ) {
+      setMonth(setDate.getMonth() + 1)
+    }
+
+    if (setDate.getFullYear() !== year) {
+      setYear(setDate.getFullYear())
+    }
+
+    setSelectedDate(setDate)
+  }
+
+  function handleSelectedFutureDateClick() {
+    const setDate = addDays(selectedDate, 1)
+
+    if ((setDate.getMonth() + 1) !== month ) {
+      setMonth(setDate.getMonth() + 1)
+    }
+
+    if (setDate.getFullYear() !== year) {
+      setYear(setDate.getFullYear())
+    }
+
+    setSelectedDate(setDate)
+  }
+
   useEffect(() => {
     function getCalendar (
     date,
@@ -117,6 +171,19 @@ const HomePage = () => {
 
     getCalendar(inputDate)
   }, [year, month])
+
+  useEffect(() => {
+    function getStartDate(
+      date
+    ) {
+      let _date = new Date(date)
+      _date.setDate(_date.getDate() - 3)
+
+      setStartDayForDayMode(_date)
+    }
+
+    getStartDate(selectedDate)
+  }, [selectedDate])
 
   return (
     <div className='w-full h-full flex flex-col'>
@@ -170,47 +237,80 @@ const HomePage = () => {
           />
         </div>
       </div>
-      <div className='grid grid-cols-7 grid-rows-[5%_repeat(6,minmax(0,1fr))] h-[98%] border-t-2 border-l-2'>
-        <div className='col-span-7 row-span-1 border-b-2 grid grid-cols-7'>
-          <div className='col-span-1 border-r-2 flex justify-center items-center'>
-          SUN
-          </div>
-          <div className='col-span-1 border-r-2 flex justify-center items-center'>
-          MON
-          </div>
-          <div className='col-span-1 border-r-2 flex justify-center items-center'>
-          TUE
-          </div>
-          <div className='col-span-1 border-r-2 flex justify-center items-center'>
-          WED
-          </div>
-          <div className='col-span-1 border-r-2 flex justify-center items-center'>
-          THU
-          </div>
-          <div className='col-span-1 border-r-2 flex justify-center items-center'>
-          FRI
-          </div>
-          <div className='col-span-1 border-r-2 flex justify-center items-center'>
-          SAT
+      <div className='grid grid-cols-7 w-full h-full'>
+        <div className='col-span-1 border-4 border-r-0 border-gray-600 rounded-l-lg grid grid-rows-[5%_repeat(7,minmax(0,1fr))_5%]'>
+          <button className='row-span-1 border-r-4 border-b-2 border-gray-600 rounded-tl bg-orange-300'
+            onClick={handleSelectedPreviousDateClick}
+          >
+            top
+          </button>
+          {Array.from({ length: 7 }, (_, index) => {
+            const date = new Date(startDayForDayMode)
+            date.setDate(startDayForDayMode.getDate() + index)
+            const isSelectedDate = toDateString(date) === toDateString(selectedDate)
+
+            return (
+            <div className='row-span-1 border-r-4 border-b-2 border-gray-600'>
+              <div className={clsx('flex flex-col justify-center items-center h-full hover:bg-yellow-200 hover:border-black cursor-pointer', { "bg-yellow-200": isSelectedDate, "text-gray-300 hover:text-gray-600": !isSelectedDate})}
+                key={date}
+                onClick={() => handleSelectedDateClick(date)}
+              >
+                <h3 className='text-lg font-extrabold'>
+                  { date.getDate() }
+                </h3>
+                <h3 className='text-lg font-extrabold'>
+                  { getDayOfWeek(date) }
+                </h3>
+              </div>
+            </div>
+            )
+          })}
+          <button className='row-span-1 border-r-4 border-gray-600 rounded-bl bg-orange-300'
+            onClick={handleSelectedFutureDateClick}
+          >
+            down
+          </button>
+        </div>
+        <div className='col-span-6 border-4 border-l-0 border-gray-600 rounded-r-lg p-2'>
+          <div className='border-4 border-gray-500 rounded-lg h-full grid grid-rows-[15%_77%_8%]'>
+            <div className='row-span-1 border-b-2 border-gray-500 flex justify-center items-center'>
+              <h2 className='text-3xl'>{`${toDateString(selectedDate)} Records`}</h2>
+            </div>
+            <div className='row-span-1 border-t-2 border-gray-500 grid grid-rows-5 p-1 gap-2'>
+              <div className='flex justify-between items-center border-4 border-slate-300 p-1 rounded-lg shadow-lg hover:bg-yellow-200 hover:shadow-slate-600 hover:border-black cursor-pointer'>
+                <h2 className='text-2xl'>Leg Day</h2>
+                <h2 className='text-2xl'>1h35min</h2>
+              </div>
+              <div className='flex justify-between items-center border-4 border-slate-300 p-1 rounded-lg shadow-lg hover:bg-yellow-200 hover:shadow-slate-600 hover:border-black cursor-pointer'>
+                <h2 className='text-2xl'>Leg Day</h2>
+                <h2 className='text-2xl'>1h35min</h2>
+              </div>
+              <div className='flex justify-between items-center border-4 border-slate-300 p-1 rounded-lg shadow-lg hover:bg-yellow-200 hover:shadow-slate-600 hover:border-black cursor-pointer'>
+                <h2 className='text-2xl'>Leg Day</h2>
+                <h2 className='text-2xl'>1h35min</h2>
+              </div>
+              <div className='flex justify-between items-center border-4 border-slate-300 p-1 rounded-lg shadow-lg hover:bg-yellow-200 hover:shadow-slate-600 hover:border-black cursor-pointer'>
+                <h2 className='text-2xl'>Leg Day</h2>
+                <h2 className='text-2xl'>1h35min</h2>
+              </div>
+              <div className='flex justify-between items-center border-4 border-slate-300 p-1 rounded-lg shadow-lg hover:bg-yellow-200 hover:shadow-slate-600 hover:border-black cursor-pointer'>
+                <h2 className='text-2xl'>Leg Day</h2>
+                <h2 className='text-2xl'>1h35min</h2>
+              </div>
+            </div>
+            <div className='row-span-1 border-t-4 border-gray-500 flex justify-center items-center gap-2 h-full'>
+              <button className='hover:bg-yellow-200'>left</button>
+              <button className='hover:bg-yellow-200'>right</button>
+            </div>
           </div>
         </div>
-        {Array.from({length: 42}, (_, index) => {
-          const date = new Date(startDate)
-          date.setDate(startDate.getDate() + index)
-
-
-          const isNotSelectedMonth = (date.getMonth() + 1) !== month
-          const isToday = toDateString(date) === toDateString(today)
-
-          return (
-            <div 
-            key={toDateString(date)}
-            className={clsx('col-span-1 row-span-1 border-r-2 border-b-2 cursor-pointer', { "text-slate-500": isNotSelectedMonth, "bg-yellow-200": isToday })}>
-              {date.getDate()}
-            </div>
-          )
-        })}
       </div>
+      <Calender
+        isHidden='hidden'
+        startDate={startDate}
+        month={month}
+        today={today}
+      ></Calender>
     </div>
   )
 }
